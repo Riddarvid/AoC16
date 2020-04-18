53dc49b2640ba1eeb28db0c79036be8aba7bdab6
riddarvid <riddarvid@gmail.com>
2020-04-18 16:38:23 +0200

    Add methods for longs

--- src/aoc/utils/input/InputUtils.java
+++ src/aoc/utils/input/InputUtils.java
@@ -22,6 +22,16 @@ public class InputUtils {
         return null;
     }
 
+    public static List<Long> getLongs(String input) {
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(input);
+        List<Long> longs = new ArrayList<>();
+        while (m.find()) {
+            longs.add(Long.parseLong(m.group()));
+        }
+        return longs;
+    }
+
     public static List<Integer> getInts(String input) {
         Pattern p = Pattern.compile("\\d+");
         Matcher m = p.matcher(input);
@@ -30,78 +40,31 @@ public class InputUtils {
             ints.add(Integer.parseInt(m.group()));
         }
         return ints;
-        /*
-        int start = 0;
-        List<Integer> ints = new ArrayList<>();
-        while (start < input.length()) {
-            while (start < input.length() && !Character.isDigit(input.charAt(start))) {
-                start++;
-            }
-            if (start >= input.length()) {
-                break;
-            }
-            int end = start + 1;
-            while (end < input.length() && Character.isDigit(input.charAt(end))) {
-                end ++;
-            }
-            ints.add(Integer.parseInt(input.substring(start, end)));
-            start = end + 1;
-        }
-        return ints;
-         */
     }
 
-    public static List<Long> getIntsNegative(String input) {
+    public static List<Integer> getIntsNegative(String input) {
         Pattern p = Pattern.compile("-?\\d+");
         Matcher m = p.matcher(input);
-        List<Long> ints = new ArrayList<>();
+        List<Integer> ints = new ArrayList<>();
         while (m.find()) {
-            ints.add(Long.parseLong(m.group()));
+            ints.add(Integer.parseInt(m.group()));
         }
         return ints;
-        /*
-        int start = 0;
-        List<Long> ints = new ArrayList<>();
-        while (start < input.length()) {
-            while (start < input.length() && !Character.isDigit(input.charAt(start)) && input.charAt(start) != '-') {
-                start++;
-            }
-            if (start >= input.length()) {
-                break;
-            }
-            int end = start + 1;
-            while (end < input.length() && Character.isDigit(input.charAt(end))) {
-                end ++;
-            }
-            long toBeAdded = Long.parseLong(input.substring(start, end));
-            ints.add(toBeAdded);
-            start = end + 1;
+    }
+
+    public static List<Long> getLongsNegative(String input) {
+        Pattern p = Pattern.compile("-?\\d+");
+        Matcher m = p.matcher(input);
+        List<Long> longs = new ArrayList<>();
+        while (m.find()) {
+            longs.add(Long.parseLong(m.group()));
         }
-        return ints;
-         */
+        return longs;
     }
 
     public static List<String> getTokens(String input, String delimiter) {
         String[] tokens = input.split(delimiter);
         return Arrays.asList(tokens);
-        /*
-        int start = 0;
-        List<String> tokens = new ArrayList<>();
-        while (start < input.length()) {
-            while (start < input.length() && input.charAt(start) == delimiter) {
-                start++;
-            }
-            if (start >= input.length()) {
-                break;
-            }
-            int end = start + 1;
-            while (end < input.length() && input.charAt(end) != delimiter) {
-                end ++;
-            }
-            tokens.add(input.substring(start, end));
-            start = end + 1;
-        }
-        return tokens;*/
     }
 
     public static List<Integer> getInts(char[] data, int start, int end) {
